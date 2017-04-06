f58ea1e77e305d6e6ad04f2bb85514235cc315cd
MO <rickymodog@yahoo.com.hk>
2017-04-07 02:29:09 +0800

    add udp broadcast search

--- app/src/main/java/com/cse/fyp/fypmobile/MainActivity.java
+++ app/src/main/java/com/cse/fyp/fypmobile/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cse.fyp.fypmobile;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -14,9 +17,21 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 
 import io.vov.vitamio.LibsChecker;
 import io.vov.vitamio.MediaPlayer;
@@ -31,17 +46,21 @@ public class MainActivity extends AppCompatActivity {
     private MediaController mc;
     private Button joyStick;
     private float dx,dy;
-    private TextView posText;
+    private TextView debugText;
     private Switch complex;
     private EditText rtmpText;
     private EditText tcpText;
     private Button rtmpBtn;
     private Button tcpBtn;
+    private Button searchBtn;
     private InetAddress ip;
-    private Thread thread;
+    private Thread tcpThread, udpThread;
     private Socket socket;
     private OutputStream os;
     private String state;
+    private String debugmsg;
+    private int port = 7689;
+
 
     private SharedPreferences sp;
     private static final String data = "DATA";
@@ -64,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.setProperty("java.net.preferIPv4Stack", "true");
         if (!LibsChecker.checkVitamioLibs(this))
             return;
         setContentView(R.layout.activity_main);
@@ -96,8 +116,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         mVideoView.start();
-
-        posText = (TextView)findViewById(R.id.posText);
+        debugText = (TextView)findViewById(R.id.debugText);
         complex = (Switch) findViewById(R.id.complexSwitch);
         rtmpText = (EditText)findViewById(R.id.rtmpText);
         tcpText = (EditText)findViewById(R.id.tcpText);
@@ -119,10 +138,11 @@ public class MainActivity extends AppCompatActivity {
         tcpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                thread = new Thread(Connection);
-                thread.start();
+                tcpThread = new Thread(Connection);
+                tcpThread.start();
             }
         });
+        /*
         if(!rtmpText.getText().toString().isEmpty())
         {
             rtmpBtn.performClick();
@@ -131,6 +151,15 @@ public class MainActivity extends AppCompatActivity {
         {
             tcpBtn.performClick();
         }
+*/
+        searchBtn = (Button)findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                udpThread = new Thread(BroadcastSearch);
+                udpThread.start();
+            }
+        });
 
         joyStick = (Button)findViewById(R.id.joystick);
         joyStick.setOnTouchListener(new View.OnTouchListener() {
@@ -169,26 +198,22 @@ public class MainActivity extends AppCompatActivity {
                 String cmd = "";
                 if(complex.isChecked())
                 {
-                    posText.setText(posX+","+ posY);
                     cmd = "m" + posX + "," + posY + "\n";
                 }
                 else
                 {
                     if(posX==0 && posY==0)
                     {
-                        posText.setText("0");
                         cmd = "";
                     }
                     else if(Math.abs(posX)>Math.abs(posY))
                     {
                         if(posX>0)
                         {
-                            posText.setText("→");
                             cmd = "d";
                         }
                         else if(posX<0)
                         {
-                            posText.setText("←");
                             cmd = "a";
                         }
                     }
@@ -196,12 +221,10 @@ public class MainActivity extends AppCompatActivity {
                     {
                         if(posY>0)
                         {
-                            posText.setText("↓");
                             cmd = "s";
                         }
                         else if(posY<0)
                         {
-                            posText.setText("↑");
                             cmd = "w";
                         }
                     }
@@ -213,9 +236,9 @@ public class MainActivity extends AppCompatActivity {
                     {
                         os.write(cmd.getBytes());
                     }
-                    catch (Exception e)
+                    catch (IOException e)
                     {
-
+                        state = "disconnected";
                     }
                 }
 
@@ -236,30 +259,104 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private Runnable Connection=new Runnable() {
+    private Runnable BroadcastSearch = new Runnable() {
         @Override
         public void run() {
             try
             {
+                InetAddress BroadcastAddress = InetAddress.getLocalHost();
+                Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 
-                ip = InetAddress.getByName(tcpText.getText().toString());
-                int port = 7689;
+                while (interfaces.hasMoreElements())
+                {
+                    NetworkInterface networkInterface = interfaces.nextElement();
+
+                    if (networkInterface.isLoopback())
+                        continue; // Don't want to broadcast to the loopback interface
+
+                    for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses())
+                    {
+                        BroadcastAddress = interfaceAddress.getBroadcast();
+
+                        if ( BroadcastAddress == null)
+                            continue;
+
+                    }
+                }
+
+                debugmsg = BroadcastAddress.getHostAddress();
+                runOnUiThread(showDebugMsg);
+                DatagramSocket udpsock =  new DatagramSocket();
+                udpsock.setBroadcast(true);
+                byte[] msg = "FYP_KHW1602_PI_REQUEST".getBytes();
+                DatagramPacket packet = new DatagramPacket(msg,msg.length,BroadcastAddress,port);
+                udpsock.send(packet);
+
+                byte[] recvBuf = new byte[1500];
+                DatagramPacket recvPacket =  new DatagramPacket(recvBuf, recvBuf.length);
+                udpsock.receive(recvPacket);
+                String recvMsg = new String(recvPacket.getData()).trim();
+                if(recvMsg.equals("FYP_KHW1602_PI_RESPONSE"))
+                {
+                    ip = recvPacket.getAddress();
+                    runOnUiThread(UpdateUI);
+                }
+                udpsock.close();
+            } catch (UnknownHostException e) {
+                debugmsg = e.getMessage();
+                runOnUiThread(showDebugMsg);
+            } catch (SocketException e) {
+                debugmsg = e.getMessage();
+                runOnUiThread(showDebugMsg);
+            } catch (IOException e) {
+                debugmsg = e.getMessage();
+                runOnUiThread(showDebugMsg);
+            }
+        }
+    };
+
+    private Runnable showDebugMsg = new Runnable() {
+        @Override
+        public void run() {
+            debugText.setText(debugmsg);
+        }
+    };
+
+    private Runnable UpdateUI = new Runnable() {
+        @Override
+        public void run() {
+            rtmpText.setText(ip.getHostAddress());
+            tcpText.setText(ip.getHostAddress());
+            rtmpBtn.performClick();
+            tcpBtn.performClick();
+        }
+    };
+
+    private Runnable Connection=new Runnable() {
+        @Override
+        public void run() {
+            if(state.equals("init") || state.equals("disconnected"))
+            {
                 try
                 {
-                    socket = new Socket(ip,port);
-                    if(socket.isConnected()) {
-                        state = "connected";
-                        os = socket.getOutputStream();
+                    ip = InetAddress.getByName(tcpText.getText().toString());
+                    try
+                    {
+                        socket = new Socket(ip,port);
+                        if(socket.isConnected()) {
+                            state = "connected";
+                            os = socket.getOutputStream();
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        state = e.getMessage().toString();
                     }
                 }
                 catch(Exception e)
                 {
                     state = e.getMessage().toString();
                 }
-            }
-            catch(Exception e)
-            {
-                state = e.getMessage().toString();
             }
         }
     };
